fca78240695c4e422c561df3da1bcabac60c2941
Fernanda Alves <fernanda__m@hotmail.com>
2020-02-15 22:00:15 -0300

    typo 2

--- images/templates/bookmarklet_launcher.js
+++ images/templates/bookmarklet_launcher.js
@@ -4,6 +4,6 @@
     }
     else {
         {% comment %} document.body.appendChild(document.createElement('script')).src='https://2e977006.ngrok.io/static/js/bookmarklet.js?r='+Math.floor(Math.random()*99999999999999999999); {% endcomment %}
-        document.body.appendChild(document.createElement('script')).src='https://memesbyfernanda.herokuapp.com'+Math.floor(Math.random()*99999999999999999999);
+        document.body.appendChild(document.createElement('script')).src='https://memesbyfernanda.herokuapp.com/static/js/bookmarklet.js?r='+Math.floor(Math.random()*99999999999999999999);
     }
 })();
