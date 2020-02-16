009fd39a27ed1a554425933dedf35a2ad3fb3838
Fernanda Alves <fernanda__m@hotmail.com>
2020-02-15 21:31:50 -0300

    settings

--- memenet/settings.py
+++ memenet/settings.py
@@ -95,8 +95,12 @@ WSGI_APPLICATION = 'memenet.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'memenet',
+        'USER': 'postgres',
+        'PASSWORD': 'admin12345',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
