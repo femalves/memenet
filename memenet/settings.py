a3b90a6809f5844f0cdc8a3fadee431f3ab6c4c6
Fernanda Alves <fernanda__m@hotmail.com>
2020-02-25 20:59:20 -0300

    removed boto

--- memenet/settings.py
+++ memenet/settings.py
@@ -213,29 +213,3 @@ STATICFILES_FINDERS = ('django.contrib.staticfiles.finders.FileSystemFinder',   
                        )
 AWS_DEFAULT_ACL = None
 AWS_D3_FILE_OVERWRITE = False
-
-# if not DEBUG:
-#     AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
-#     AWS_S3_REGION_NAME = 'us-east-2'  # e.g. us-east-2
-#     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-#     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-#     AWS_HEADERS = {'Cache-Control': 'max-age=86400', }
-#     AWS_S3_FILE_OVERWRITE = False
-#     AWS_DEFAULT_ACL = None
-
-#     # Tell django-storages the domain to use to refer to static files.
-#     AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-
-#     # Tell the staticfiles app to use S3Boto3 storage when writing the collected static files (when
-#     # you run `collectstatic`).
-#     STATICFILES_LOCATION = 'static'
-#     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
-
-#     MEDIAFILES_LOCATION = 'media'
-#     DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
-# else:
-#     STATIC_URL = '/static/'
-#     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-#     MEDIA_URL = '/media/'
-#     MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
