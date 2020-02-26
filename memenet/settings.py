56824063361141f988c0c8108008117d1caf0429
Fernanda Alves <fernanda__m@hotmail.com>
2020-02-25 22:28:16 -0300

    settings

--- memenet/settings.py
+++ memenet/settings.py
@@ -210,7 +210,7 @@ STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 # ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
-STATICFILES_FINDERS = ('django.contrib.staticfiles.finders.FileSystemFinder',    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-                       )
+STATICFILES_FINDERS = ('django.contrib.staticfiles.finders.FileSystemFinder',
+                       'django.contrib.staticfiles.finders.AppDirectoriesFinder')
 AWS_DEFAULT_ACL = None
 AWS_D3_FILE_OVERWRITE = False
