607c387bd9ecaf49874db0f9971f8baa96f669f7
Fernanda Alves <fernanda__m@hotmail.com>
2020-02-25 21:17:36 -0300

    s3

--- memenet/settings.py
+++ memenet/settings.py
@@ -205,7 +205,8 @@ AWS_S3_OBJECT_PARAMETERS = {
 }
 
 DEFAULT_FILE_STORAGE = 'memenet.custom_storages.MediaStorage'
-STATICFILES_STORAGE = "storages.backends.s3boto.S3BotoStorage"
+STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+
 
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 # ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
